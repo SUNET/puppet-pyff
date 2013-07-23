e5fd31255979bd0d54e7e81dce058a2c95535cc9
Leif Johansson <leifj@sunet.se>
2013-07-23 23:56:52 +0200

    import

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name		'puppet-pyff'
+version 	'1.0.0'
+
+author  	'Leif Joahansson'
+license 	'BSD'
+project_page	'https://github.com/samlbits/puppet-pyff'
+source		'git://github.com/samlbits/puppet-pyff'
+summary		'A puppet module for pyFF'
+description	'Installs and manages pyFF in a virtualenvj
---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+class pyff ($dir = '/opt/pyff') {
+  include python
+  python::virtualenv { $dir:
+    ensure => present,
+  }
+  python::pip { 'pyff':
+    virtualenv => $dir,
+  }
+  file {'pyffd-upstart':
+    ensure    => file,
+    path      => '/etc/init/pyffd.conf',
+    content   => template('pyff/pyffd-upstart.erb'),
+    notify    => Service['pyffd']
+  }
+  file {'pyffd-defaults':
+    ensure    => file,
+    path      => '/etc/default/pyffd',
+    content   => template('pyff/pyffd-defaults.erb'),
+    notify    => Service['pyffd']
+  }
+  service {'pyffd':
+    ensure    => 'running',
+    require   => [File['pyffd-upstart'],File['pyffd-defaults']]
+  }
+}
